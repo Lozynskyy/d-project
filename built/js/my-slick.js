3984a509286ebe7122a0b797d6f52fd0f71199d7
Vitaliy Lozynskiy <lozynskiy.v@gmail.com>
2018-05-27 01:15:35 +0300

    Adapt for 1024-1280px

--- built/js/my-slick.js
+++ built/js/my-slick.js
@@ -1,6 +1,15 @@
-$('.team__slider__body').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    nextArrow: "<img src='../img/interface---arrow-right-b.png'>",
-    prevArrow: "<img src='../img/interface---arrow-left-b.png'>"
-});
+if(document.documentElement.clientWidth > 1440){
+    $('.team__slider__body').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        nextArrow: "<img src='../img/interface---arrow-right-b.png'>",
+        prevArrow: "<img src='../img/interface---arrow-left-b.png'>"
+    });
+} else if(document.documentElement.clientWidth <= 1440) {
+    $('.team__slider__body').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        nextArrow: "<img src='../img/interface---arrow-right-b.png'>",
+        prevArrow: "<img src='../img/interface---arrow-left-b.png'>"
+    });
+}
