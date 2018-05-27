c772f0773416c3bb2d356fd9fd7b6e9a3cd9a287
Vitaliy Lozynskiy <lozynskiy.v@gmail.com>
2018-05-27 13:25:02 +0300

    Add responsive 800-1024px

--- built/js/my-slick.js
+++ built/js/my-slick.js
@@ -2,14 +2,14 @@ if(document.documentElement.clientWidth > 1440){
     $('.team__slider__body').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        nextArrow: "<img src='../img/interface---arrow-right-b.png'>",
-        prevArrow: "<img src='../img/interface---arrow-left-b.png'>"
+        nextArrow: "<img src='img/interface---arrow-right-b.png'>",
+        prevArrow: "<img src='img/interface---arrow-left-b.png'>"
     });
 } else if(document.documentElement.clientWidth <= 1440) {
     $('.team__slider__body').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
-        nextArrow: "<img src='../img/interface---arrow-right-b.png'>",
-        prevArrow: "<img src='../img/interface---arrow-left-b.png'>"
+        nextArrow: "<img src='img/interface---arrow-right-b.png'>",
+        prevArrow: "<img src='img/interface---arrow-left-b.png'>"
     });
 }
